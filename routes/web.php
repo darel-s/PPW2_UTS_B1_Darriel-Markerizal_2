8429e40afe0555d4810bcb7ea8a3c1db5e52373d
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-10-20 10:51:52 +0700

    Selesai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/products');
 });
-Route::resource('products', Controller::class);
+Route::resource('products', ProductController::class);
